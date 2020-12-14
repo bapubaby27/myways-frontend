5bb219422cb085736601795a5603c19bd11b2d56
bapubaby27 <nimumehta83@gmail.com>
2020-12-14 12:41:30 +0530

    Form Validation Added

--- src/components/LoginDrawer/LoginDrawer.js
+++ src/components/LoginDrawer/LoginDrawer.js
@@ -15,10 +15,10 @@ const LoginDrawer = ({ changeType, toggleDrawer, changeUser }) => {
   const [loading, setLoading] = useState(false);
   const [recievedOtp, setRecievedOtp] = useState(0);
   const [enteredOtp, setEnteredOtp] = useState("");
+  const [showEmailAndPassword, setShowEmailAndPassword] = useState(true);
   const [showEmail, setShowEmail] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [showOtp, setShowOtp] = useState(false);
-  const [success, setSuccess] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
 
   const handleChange = (e) => {
@@ -28,19 +28,73 @@ const LoginDrawer = ({ changeType, toggleDrawer, changeUser }) => {
     });
   };
 
-  console.log(showEmail);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (showEmailAndPassword) {
+      if (
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          formData.email
+        )
+      ) {
+        setLoading(true);
+        let user = await signInUser(formData);
+        setLoading(false);
+        if (!user.data.status) {
+          setError(user.data.message);
+        } else {
+          changeUser(user.data.data);
+          toggleDrawer(false);
+        }
+      } else {
+        setError("Enter a valid Email");
+      }
+    } else if (showEmail) {
+      if (
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          formData.email
+        )
+      ) {
+        setLoading(true);
+        const res = await sendOtp(formData.email);
+        setLoading(false);
+        setRecievedOtp(res.data.otp);
+        setShowOtp(true);
+        setShowEmail(false);
+      } else {
+        setError("Enter a valid Email");
+      }
+    } else if (showOtp) {
+      if (parseInt(enteredOtp) === recievedOtp) {
+        setShowOtp(false);
+        setShowPassword(true);
+      } else {
+        setError("Please Enter a valid OTP");
+      }
+    } else {
+      setLoading(true);
+      let res = await updatePassword(formData);
+      setLoading(false);
+      setShowPassword(false);
+      setSuccessMessage(res.data.message);
+      setFormData({
+        email: "",
+        password: "",
+      });
+      setEnteredOtp("");
+      setShowEmailAndPassword(true);
+      setTimeout(() => setSuccessMessage(""), 2000);
+    }
+  };
 
   return (
     <>
       <Backdrop />
       <div className={loading ? "login login--loader" : "login"}>
         <div className="login__mainHeader">
-          {showEmail || showPassword || showOtp || success || loading ? null : (
-            <h1>Login</h1>
-          )}
+          {!showEmailAndPassword ? null : <h1>Login</h1>}
           <span onClick={() => toggleDrawer(false)}>&#10005;</span>
         </div>
-        {!loading && !success ? (
+        {!loading ? (
           <>
             <div className="login__form">
               <div className="login__header">
@@ -54,117 +108,77 @@ const LoginDrawer = ({ changeType, toggleDrawer, changeUser }) => {
                   <h2>Student</h2>
                 )}
               </div>
-              {(showEmail || !showPassword) && !showOtp && (
-                <input
-                  className="login__input"
-                  type="email"
-                  value={formData.email}
-                  name="email"
-                  placeholder="Email"
-                  onChange={handleChange}
-                />
-              )}
-              {(!showEmail || showPassword) && !showOtp && (
-                <input
-                  className="login__input"
-                  type="password"
-                  value={formData.password}
-                  name="password"
-                  placeholder="Password"
-                  onChange={handleChange}
-                />
-              )}
-              {showOtp && (
-                <input
-                  className="login__input"
-                  type="text"
-                  value={enteredOtp}
-                  name="OTP"
-                  placeholder="Enter OTP"
-                  onChange={(e) => setEnteredOtp(e.target.value)}
-                />
-              )}
-              {!showEmail && !showPassword && !showOtp && (
-                <p
-                  style={{ cursor: "pointer" }}
-                  onClick={() => setShowEmail(true)}
-                >
-                  Forgot Password?
-                </p>
-              )}
-              {error.length > 0 && (
-                <p style={{ color: "red", textAlign: "center" }}>{error}</p>
-              )}
-              {!showEmail && !showPassword && !showOtp && (
+              <form onSubmit={handleSubmit}>
+                {(showEmailAndPassword || showEmail) && (
+                  <input
+                    className="login__input"
+                    type="email"
+                    value={formData.email}
+                    name="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                    required
+                  />
+                )}
+                {(showEmailAndPassword || showPassword) && (
+                  <input
+                    className="login__input"
+                    type="password"
+                    value={formData.password}
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    required
+                  />
+                )}
+                {showOtp && (
+                  <input
+                    className="login__input"
+                    type="text"
+                    value={enteredOtp}
+                    name="OTP"
+                    placeholder="Enter OTP"
+                    onChange={(e) => setEnteredOtp(e.target.value)}
+                    required
+                  />
+                )}
+                {showEmailAndPassword && (
+                  <p
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                      setShowEmail(true);
+                      setShowEmailAndPassword(false);
+                    }}
+                  >
+                    Forgot Password?
+                  </p>
+                )}
+                {error.length > 0 && (
+                  <p style={{ color: "red", textAlign: "center" }}>{error}</p>
+                )}
+                {successMessage.length > 0 && (
+                  <p style={{ color: "green", textAlign: "center" }}>
+                    {successMessage}
+                  </p>
+                )}
                 <Button
-                  onClick={async () => {
-                    setLoading(true);
-                    let user = await signInUser(formData);
-                    setLoading(false);
-                    if (!user.data.status) {
-                      setError(user.data.message);
-                    } else {
-                      changeUser(user.data.data);
-                      toggleDrawer(false);
-                    }
-                  }}
-                  versions={["green", "border3"]}
+                  type="submit"
+                  versions={["green", "border3", "fullWidth"]}
                 >
-                  <span style={{ fontSize: "1.5rem" }}>Login</span>
+                  <span style={{ fontSize: "1.5rem" }}>
+                    {showEmailAndPassword
+                      ? "Login"
+                      : showEmail
+                      ? "Send OTP"
+                      : showOtp
+                      ? "Submit OTP"
+                      : showPassword
+                      ? "Submit Password"
+                      : null}
+                  </span>
                 </Button>
-              )}
-              {showEmail && (
-                <Button
-                  onClick={async () => {
-                    setLoading(true);
-                    const user = await sendOtp(formData.email);
-                    setRecievedOtp(user.data.otp);
-                    setLoading(false);
-                    if (!user.data.status) {
-                      setError(user.data.message);
-                    } else {
-                      setShowEmail(false);
-                      setShowOtp(true);
-                    }
-                  }}
-                  versions={["green", "border3"]}
-                >
-                  <span style={{ fontSize: "1.5rem" }}>Send OTP</span>
-                </Button>
-              )}
-              {showOtp && (
-                <Button
-                  onClick={async () => {
-                    if (parseInt(enteredOtp) === recievedOtp) {
-                      setShowOtp(false);
-                      setShowPassword(true);
-                    } else {
-                      setError("Please Enter a valid OTP");
-                    }
-                  }}
-                  versions={["green", "border3"]}
-                >
-                  <span style={{ fontSize: "1.5rem" }}>Submit OTP</span>
-                </Button>
-              )}
-              {showPassword && (
-                <Button
-                  onClick={async () => {
-                    setLoading(true);
-                    let user = await updatePassword(formData);
-                    setLoading(false);
-                    setShowPassword(false);
-                    if (user.data.status) {
-                      setSuccess(true);
-                      setSuccessMessage(user.data.message);
-                    }
-                  }}
-                  versions={["green", "border3"]}
-                >
-                  <span style={{ fontSize: "1.5rem" }}>Submit Password</span>
-                </Button>
-              )}
-              {!showEmail && !showPassword && !showOtp && (
+              </form>
+              {showEmailAndPassword && (
                 <a
                   onClick={() => {
                     toggleDrawer(false);
@@ -176,12 +190,10 @@ const LoginDrawer = ({ changeType, toggleDrawer, changeUser }) => {
               )}
             </div>
           </>
-        ) : !success ? (
+        ) : (
           <div className="login__loader">
             <Loader type="Circles" color="#00BFFF" height={80} width={80} />
           </div>
-        ) : (
-          <p className="login__loader">{successMessage}</p>
         )}
       </div>
     </>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -22,6 +22,41 @@ const Modal = ({ type, changeType, changeUser }) => {
       [e.target.name]: e.target.value,
     });
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        formData.email
+      )
+    ) {
+      changeType("loading");
+      const res = await sendOtp(formData.email);
+      setRecievedOtp(res.data.otp);
+      changeType("otp");
+    } else {
+      setError("Enter a valid Email");
+      setTimeout(() => setError(""), 2000);
+    }
+  };
+
+  const handleOtp = async (e) => {
+    e.preventDefault();
+    if (parseInt(enteredOtp) === recievedOtp) {
+      changeType("loading");
+      const user = await setUser(formData);
+      if (user.data.status) {
+        changeUser(user.data.data);
+        changeType("success");
+        setTimeout(() => changeType(""), 2000);
+      } else {
+        setError(user.data.message);
+      }
+    } else {
+      setError("Please Enter a valid OTP");
+    }
+  };
+
   return (
     <>
       <Backdrop />
@@ -34,54 +69,63 @@ const Modal = ({ type, changeType, changeUser }) => {
             <div className="modal__form">
               <h1 style={{ textAlign: "center" }}>Sign Up</h1>
               <p style={{ textAlign: "center" }}>It's quick and easy</p>
-              <div className="modal__inputs">
+              <form onSubmit={handleSubmit}>
+                <div className="modal__inputs">
+                  <input
+                    className="modal__input"
+                    type="text"
+                    value={formData.firstName}
+                    name="firstName"
+                    placeholder="First Name"
+                    onChange={handleChange}
+                    required
+                  />
+                  <input
+                    className="modal__input"
+                    type="text"
+                    value={formData.lastName}
+                    name="lastName"
+                    placeholder="Last Name"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
                 <input
-                  className="modal__input"
-                  type="text"
-                  value={formData.firstName}
-                  name="firstName"
-                  placeholder="First Name"
+                  className="login__input"
+                  type="email"
+                  value={formData.email}
+                  name="email"
+                  placeholder="Email"
                   onChange={handleChange}
+                  required
                 />
                 <input
-                  className="modal__input"
-                  type="text"
-                  value={formData.lastName}
-                  name="lastName"
-                  placeholder="Last Name"
+                  className="login__input"
+                  type="password"
+                  value={formData.password}
+                  name="password"
+                  placeholder="Password"
                   onChange={handleChange}
+                  required
                 />
-              </div>
-              <input
-                className="login__input"
-                type="email"
-                value={formData.email}
-                name="email"
-                placeholder="Email"
-                onChange={handleChange}
-              />
-              <input
-                className="login__input"
-                type="password"
-                value={formData.password}
-                name="password"
-                placeholder="Password"
-                onChange={handleChange}
-              />
-              {error.length !== 0 ? (
-                <p style={{ color: "red" }}>{error}</p>
-              ) : null}
-              <Button
-                onClick={async () => {
-                  changeType("loading");
-                  const res = await sendOtp(formData.email);
-                  setRecievedOtp(res.data.otp);
-                  changeType("otp");
-                }}
-                versions={["green", "border3"]}
-              >
-                <span style={{ fontSize: "1.5rem" }}>Sign Up</span>
-              </Button>
+                {error.length !== 0 ? (
+                  <p
+                    style={{
+                      color: "red",
+                      marginBottom: "10px",
+                      fontSize: "16px",
+                    }}
+                  >
+                    {error}
+                  </p>
+                ) : null}
+                <Button
+                  type="submit"
+                  versions={["green", "border3", "fullWidth"]}
+                >
+                  <span style={{ fontSize: "1.5rem" }}>Sign Up</span>
+                </Button>
+              </form>
             </div>
           </>
         ) : type === "otp" ? (
@@ -92,37 +136,23 @@ const Modal = ({ type, changeType, changeUser }) => {
             <div className="otp">
               <p>OTP sent!</p>
               {error.length > 0 && <p style={{ color: "red" }}>{error}</p>}
-              <input
-                className="modal__input modal__input--round"
-                type="text"
-                value={enteredOtp}
-                name="otp"
-                placeholder="Enter OTP"
-                onChange={(e) => setEnteredOtp(e.target.value)}
-              />
-              <p style={{ fontSize: "1rem", textAlign: "left" }}>
-                One time Passcode sent to your email ID - {formData.email}
-              </p>
-              <Button
-                onClick={async () => {
-                  if (parseInt(enteredOtp) === recievedOtp) {
-                    changeType("loading");
-                    const user = await setUser(formData);
-                    if (user.data.status) {
-                      changeUser(user.data.data);
-                      changeType("success");
-                      setTimeout(() => changeType(""), 2000);
-                    } else {
-                      setError(user.data.message);
-                    }
-                  } else {
-                    setError("Please Enter a valid OTP");
-                  }
-                }}
-                versions={["green", "border2", "large"]}
-              >
-                <span style={{ fontSize: "1.5rem" }}>Enter</span>
-              </Button>
+              <form onSubmit={handleOtp}>
+                <input
+                  className="modal__input modal__input--round"
+                  type="text"
+                  value={enteredOtp}
+                  name="otp"
+                  placeholder="Enter OTP"
+                  onChange={(e) => setEnteredOtp(e.target.value)}
+                  required
+                />
+                <p style={{ fontSize: "1rem", textAlign: "left" }}>
+                  One time Passcode sent to your email ID - {formData.email}
+                </p>
+                <Button versions={["green", "border2", "large"]}>
+                  <span style={{ fontSize: "1.5rem" }}>Enter</span>
+                </Button>
+              </form>
             </div>
           </>
         ) : type === "success" ? (
